66abfee60328b8eaa4da84c4bcc1d563099c4d8f
TikhonP <tikhon.petrishchev@gmail.com>
2025-03-14 21:13:58 +0300

    Testing wifi endpoint connection

--- main/telepat-pill-dispenser.c
+++ main/telepat-pill-dispenser.c
@@ -1,6 +1,11 @@
 #include "driver/gpio.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h" // IWYU pragma: export
+#include "nvs_flash.h"
+#include "wifi.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "http-request.h"
 
 #define TAG "telepat-pill-dispenser"
 
@@ -8,6 +13,21 @@
 
 void app_main(void) {
     ESP_LOGI(TAG, "Hello world!");
+
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    wifi_init_sta();
+
+    xTaskCreate(&http_test_task, "http_test_task", 8192, NULL, 5, NULL);
 
     gpio_reset_pin(LED_PIN);
     gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
@@ -19,4 +39,3 @@ void app_main(void) {
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
-
